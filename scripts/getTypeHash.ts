10a06496714b94bc2dd516bcb1b65f1956852425
maanas <maanas2605@gmail.com>
2023-05-09 23:02:18 +0530

    feat: normal and multiasset orders

---
+++ scripts/getTypeHash.ts
@@ -0,0 +1,57 @@
+import { toUtf8Bytes } from "ethers/lib/utils";
+import { ethers } from "ethers";
+
+function getStringKeccakHash(item: string): string {
+  return ethers.utils.keccak256(toUtf8Bytes(item));
+}
+
+let NormalOrder = `struct NormalOrder {
+  address seller;
+  address buyer;
+  address sellToken;
+  address buyToken;
+  uint256 sellAmount;
+  uint256 buyAmount;
+  uint256 expiry;
+}`;
+
+let MultiAssetOrder = `struct MultiAssetOrder {
+  address seller;
+  address buyer;
+  address sellToken;
+  uint256 sellAmount;
+  bytes32 buyAssetMixHash;
+  uint256 expiry;
+}`;
+
+function parseSolidityStruct(solidityStruct: string): string {
+  let parsedString: string = "";
+
+  let words = solidityStruct.match(/[a-zA-Z0-9]+/g);
+  if (!words) {
+    throw new Error("Invalid strcut string");
+  }
+  parsedString += words[1];
+  parsedString += "(";
+  for (let i = 2; i < words.length; i++) {
+    if (i % 2 == 0) {
+      parsedString += words[i];
+      parsedString += " ";
+    } else {
+      parsedString += words[i];
+      parsedString += ",";
+    }
+  }
+
+  return parsedString.substring(0, parsedString.length - 1) + ")";
+}
+
+const typeHashNormalOrder = getStringKeccakHash(
+  parseSolidityStruct(NormalOrder)
+);
+const typeHashMultiAssetOrder = getStringKeccakHash(
+  parseSolidityStruct(MultiAssetOrder)
+);
+
+console.log("Normal Order : ", typeHashNormalOrder);
+console.log("MultiAsset Order : ", typeHashMultiAssetOrder);
